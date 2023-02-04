1d158be7d1307ef05a4ec4164b99defb91069488
Omarghalayini <42498579+Omarghalayini@users.noreply.github.com>
2023-02-04 17:05:15 +0000

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,39 @@
+ #include <iostream>
+
+ using namespace std;
+
+ int main(){
+    
+double area, perimeter, breadth, length;
+
+cout << " Please Enter your length and breadth";
+cin >> length >> breadth;
+
+perimeter = 2 * (length + breadth);
+
+cout << "perimeter is: " << perimeter << endl;
+
+area = length * breadth;
+
+cout << "area is: " << area; 
+
+
+
+
+
+
+    return 0;
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
